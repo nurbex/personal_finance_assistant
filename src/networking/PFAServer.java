8e2c5a2333818c0b73f9aa24c4d6c56212b246ed
Nurbek <nurbekaj@gmail.com>
2021-03-28 19:42:35 -0500

    data is accessed according to user info
    user authentication was added, data is accessed according to user info

--- src/networking/PFAServer.java
+++ src/networking/PFAServer.java
@@ -2,26 +2,30 @@ package networking;
 
 import domain.Expense;
 import domain.Income;
+import domain.User;
 import services.ExpenseService;
 import services.IncomeService;
+import services.UserService;
 import ui.UI;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class PFAServer {
     public static void main(String[] args) {
 
         UI ui = new UI();
+        UserService userService= new UserService();
         IncomeService incomeService = new IncomeService();
         ExpenseService expenseService = new ExpenseService();
-
         try (ServerSocket serverSocket = new ServerSocket(8888)) {
 
             System.out.println("Server is up and running...");
+            String userName;
+            String password;
 
             Socket incoming = serverSocket.accept();
             //read data from connected socket
@@ -36,71 +40,86 @@ public class PFAServer {
             if (incoming.isConnected()) {
                 System.out.println("Somebody is just connected! "+ incoming.getInetAddress().toString());
             }
+            writeToWriterAndFlush(ui.loginMenu(), out);
+            userName=in.nextLine();
+            password=in.nextLine();
 
-            Integer input ;
-            do {
-                writeToWriterAndFlush(ui.mainMenu(), out);
-                input = in.nextInt();
+            Optional<User> optionalUser = userService.getUserByEmailAndPassword(userName, password);
 
-                if (input == -1)
-                    break;
-                switch (input) {
-                    case 1:
-                        writeToWriterAndFlush(ui.newIncomeMenu(), out);
-                        String incomeName = in.next();
-                        float incomeValue = in.nextFloat();
+            if (optionalUser.isPresent()) {
+                System.out.println("user is present");
+                User user = optionalUser.get();
+                Integer input ;
+                do {
+                    writeToWriterAndFlush(ui.mainMenu(), out);
+                    input = in.nextInt();
 
-                        if (incomeValue == -1 || incomeName.equals("-1"))
+                    if (input == -1)
+                        break;
+                    switch (input) {
+                        case 1:
+                            writeToWriterAndFlush(ui.newIncomeMenu(), out);
+                            String incomeName = in.next();
+                            float incomeValue = in.nextFloat();
+
+                            if (incomeValue == -1 || incomeName.equals("-1"))
+                                break;
+
+                            Income income = new Income(incomeName, incomeValue, LocalDateTime.now(),user);
+
+                            if (incomeService.insert(income))
+                                writeToWriterAndFlush("Your income recorded as "+incomeService.numberOfIncomes(), out);
                             break;
+                        case 2:
+                            writeToWriterAndFlush(ui.newExpenseMenu(), out);
 
-                        Income income = new Income(incomeName, incomeValue, LocalDateTime.now());
+                            String expenseName = in.next();
+                            float expenseValue = in.nextFloat();
 
-                        if (incomeService.insert(income))
-                            writeToWriterAndFlush("Your income recorded as "+incomeService.numberOfIncomes(), out);
-                        break;
-                    case 2:
-                        writeToWriterAndFlush(ui.newExpenseMenu(), out);
+                            if (expenseValue == -1 || expenseName.equals("-1"))
+                                break;
 
-                        String expenseName = in.next();
-                        float expenseValue = in.nextFloat();
+                            Expense expense =  new Expense(expenseName, expenseValue, LocalDateTime.now(), user);
 
-                        if (expenseValue == -1 || expenseName.equals("-1"))
+                            if (expenseService.insert(expense))
+                                writeToWriterAndFlush("Your expense recorded as "+expenseService.numberOfExpense(), out);
+
                             break;
+                        case 3:
+                            writeToWriterAndFlush("List income is selected", out);
+                            writeToWriterAndFlush(incomeService.getData(user).toString(), out);
+                            break;
+                        case 4:
+                            writeToWriterAndFlush("List expense is selected", out);
+                            writeToWriterAndFlush(expenseService.getData(user).toString(), out);
+                            break;
+                        case 5:
+                            Float incomeSum = incomeService.calculateSum(incomeService.getIncomesOfGivenDate(LocalDateTime.now(), user));
+                            Float expenseSum = expenseService.calculateSum(expenseService.getExpensesOfGivenDate(LocalDateTime.now(), user));
 
-                        Expense expense =  new Expense(expenseName, expenseValue, LocalDateTime.now());
+                            writeToWriterAndFlush(incomeService.getIncomesOfGivenDate(LocalDateTime.now(), user).toString(), out);
+                            writeToWriterAndFlush(expenseService.filterListByDate(LocalDateTime.now()).toString(), out);
 
-                        if (expenseService.insert(expense))
-                            writeToWriterAndFlush("Your expense recorded as "+expenseService.numberOfExpense(), out);
+                            writeToWriterAndFlush("sum of your incomes for month: "+LocalDateTime.now().getMonth()+" : "
+                                    + incomeSum, out);
 
-                        break;
-                    case 3:
-                        writeToWriterAndFlush("List income is selected", out);
-                        writeToWriterAndFlush(incomeService.toString(), out);
-                        break;
-                    case 4:
-                        writeToWriterAndFlush("List expense is selected", out);
-                        writeToWriterAndFlush(expenseService.toString(), out);
-                        break;
-                    case 5:
-                        Float incomeSum = incomeService.calculateSum(incomeService.getIncomesOfGivenDate(LocalDateTime.now()));
-                        Float expenseSum = expenseService.calculateSum(expenseService.getExpensesOfGivenDate(LocalDateTime.now()));
+                            writeToWriterAndFlush("sum of expenses for month: "+LocalDateTime.now().getMonth()+ " : "
+                                    + expenseSum, out);
+                            writeToWriterAndFlush("Your balance is : "+ (incomeSum - expenseSum), out);
+                            break;
+                        default:
+                            writeToWriterAndFlush("please use numbers between 1 and 4", out);
+                            break;
+                    }
 
-                        writeToWriterAndFlush(incomeService.getIncomesOfGivenDate(LocalDateTime.now()).toString(), out);
-                        writeToWriterAndFlush(expenseService.filterListByDate(LocalDateTime.now()).toString(), out);
+                } while (input != -1);
 
-                        writeToWriterAndFlush("sum of your incomes for month: "+LocalDateTime.now().getMonth()+" : "
-                                + incomeSum, out);
-
-                        writeToWriterAndFlush("sum of expenses for month: "+LocalDateTime.now().getMonth()+ " : "
-                                + expenseSum, out);
-                        writeToWriterAndFlush("Your balance is : "+ (incomeSum - expenseSum), out);
-                        break;
-                    default:
-                        writeToWriterAndFlush("please use numbers between 1 and 4", out);
-                        break;
-                }
-
-            } while (input != -1);
+            }
+            else{
+                writeToWriterAndFlush(ui.noSucchUser(),out);
+                System.out.println(ui.noSucchUser());
+                System.exit(0);
+            }
 
             out.close();
             in.close();
